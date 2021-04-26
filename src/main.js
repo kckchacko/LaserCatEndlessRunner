b5196190b23c2a93856815864d83dbacd6da3813
noorhaider702 <mnhaider@ucsc.edu>
2021-04-25 19:29:41 -0700

    replacing files

--- src/main.js
+++ src/main.js
@@ -1,52 +1,35 @@
 // Nathan Altice
-// Paddle Parkour P3
-// An endless dodging game
-// Barrier prefab adapted from Travis Faas, An Introduction to HTML5 Game Development with Phaser.js (2017)
-// Original: 4/20/17 (Phaser CE version)
-// Updated: 4/23/21
+// Updated: 7/1/20
+// Phaser 3 Movement Studies
+// Concepts: Arcade physics, atlas and atlasXML loading, atlas animation (custom and generated frames), physics world wrapping, physics body properties (velocity, acceleration, drag, max acceleration), keyboard (isDown, JustPressed, DownDuration, UpDuration)
+// Some mechanics inspired by and adapted from Game Mechanic Explorer https://gamemechanicexplorer.com
+// The two example atlases  are commercial assets and should not be used for your own projects - buy them from https://www.kenney.nl/assets :)
 
-// keep me honest
+// tame the javashrek
 'use strict';
 
-// define and configure main Phaser game object
+// global variables
+let cursors;
+let currentScene = 0;
+const SCALE = 0.5;
+const tileSize = 35;
+
+// main game object
 let config = {
-    parent: 'myGame',
-    type: Phaser.AUTO,
-    height: 640,
-    width: 960,
-    scale: {
-        autoCenter: Phaser.Scale.CENTER_BOTH
-    },
+    type: Phaser.WEBGL,
+    width: 840,
+    height: 525,
     physics: {
         default: 'arcade',
         arcade: {
-            //debug: true,
+            debug: true,
             gravity: {
                 x: 0,
                 y: 0
             }
         }
     },
-    scene: [ Load, Title, Play, GameOver ]
-}
+    scene: [ Load, Velocity]
+};
 
-// uncomment the following line if you need to purge local storage data
-//localStorage.clear();
-
-// define game
 let game = new Phaser.Game(config);
-
-// define globals
-let centerX = game.config.width/2;
-let centerY = game.config.height/2;
-let w = game.config.width;
-let h = game.config.height;
-const textSpacer = 64;
-let paddle = null;
-const paddleWidth = 16;
-const paddleHeight = 128;
-const paddleVelocity = 150;
-let level;
-let highScore;
-let newHighScore = false;
-let cursors;
